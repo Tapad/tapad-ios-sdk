2837da2b8e17bed5259b12b07ed848c14f6c168a
Li Qiu <lqiu@alumni.cmu.edu>
2012-06-21 18:09:22 -0400

    reorganized repo for better readability and added #defines for excluding code for unused identifiers

---
+++ src/non-arc/event-tracking/MyEnabledTapadIdentifiers.h
@@ -0,0 +1,22 @@
+//
+//  MyEnabledTapadIdentifiers.h
+//
+//  Edit this header file to enable identifiers allowed for your app
+//
+//  Created by Li Qiu on 6/20/2012.
+//  Copyright 2012 Tapad, Inc. All rights reserved.
+//
+
+// list of possible identifiers to enable
+// uncomment any identifiers that the app will be using
+// this is a stub file - see copy and usage in demo app
+
+//#define TAPAD_IDENTIFIER_ENABLE_OPENUDID
+
+//#define TAPAD_IDENTIFIER_ENABLE_MD5_HASHED_MAC
+
+//#define TAPAD_IDENTIFIER_ENABLE_SHA1_HASHED_MAC
+
+//#define TAPAD_IDENTIFIER_ENABLE_MD5_HASHED_UDID
+
+//#define TAPAD_IDENTIFIER_ENABLE_SHA1_HASHED_UDID
